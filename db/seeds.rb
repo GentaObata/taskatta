b78d35d0c0f2cf8bbf7326b0b3454c172126a8ff
obata <g.obatal@mof-mof.co.jp>
2021-04-12 01:19:23 +0900

    add create_statuses_table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,23 @@
+TaskStatus.create!(
+  id: 10,
+  name: 'waiting'
+)
+TaskStatus.create!(
+  id: 20,
+  name: 'doing'
+)
+TaskStatus.create!(
+  id: 30,
+  name: 'done'
+)
+
 10.times do |n|
   title = "task-#{n+1}"
   due_time = Time.current.since(n.days)
   Task.create!(
     title: title,
     body: "puipui",
-    due_time: due_time 
+    due_time: due_time,
+    task_statuses_id: 10
   )
 end
